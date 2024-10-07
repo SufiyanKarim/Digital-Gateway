cdc0687e23fa57f928ba46d8d3f6a734aba972bc
SufiyanKarim <sufiib165@gmail.com>
2024-10-06 23:00:50 -0700

    update code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from '../../components/hero/Hero'
-import KeyFeatures from "../../components/keyFeatures/KeyFeatures";
+import KeyFeatures from '../../components/keyFeatures/KeyFeatures'
 import ServicesOverview from "../../components/servicesOverview/ServicesOverview";
 import Testimonials from "../../components/testimonials/Testimonials";
 
