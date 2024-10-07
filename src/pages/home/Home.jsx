d26b7d4cf12b2c14fa4d4cd4a94ca6ba85b149c3
SufiyanKarim <sufiib165@gmail.com>
2024-10-06 22:45:38 -0700

    update code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Hero from "../../components/Hero/Hero";
+import Hero from '../../components/hero/Hero'
 import KeyFeatures from "../../components/KeyFeatures/KeyFeatures";
 import ServicesOverview from "../../components/servicesOverview/ServicesOverview";
 import Testimonials from "../../components/testimonials/Testimonials";
 
-
 const Home = () => {
-  return <div>
-     
-     <Hero/>
-     <KeyFeatures/>
-     <ServicesOverview/>
-      <Testimonials/>
-
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <KeyFeatures />
+      <ServicesOverview />
+      <Testimonials />
+    </div>
+  );
 };
 
 export default Home;
